cd8132885a681ddcbc01e1f88b4e6c2e16ec2248
alexmascension <alexmascension@gmail.com>
2024-12-06 17:21:15 +0100

    Processed files

---
+++ src/version_2/list_vars.py
@@ -0,0 +1,12 @@
+
+DATA_DIR = '../../data'
+RESULTS_DIR = '../../results'
+DIR_SUMMARY_OUTPUT = '../../results/summary/'
+DIR_PROFILING = '../../results/profiling'
+DIR_FIGURES = '../../results/figures'
+
+POOLS = [f'POOL{i}' for i in range(1, 13)]
+CONTROLS = ['ACIDOLA', 'BLACTIS']
+LIST_PROFILERS = ['centrifuge', 'ganon', 'kaiju', 'kmcp', 'kraken2', 'krakenuniq']
+
+DPI=300
